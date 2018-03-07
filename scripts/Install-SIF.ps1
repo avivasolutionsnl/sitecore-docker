6aabbe6adc5a0b3d29f200b1565cc3c5b7a077fd
Joost Meijles <joost.meijles@avivasolutions.nl>
2018-03-07 15:30:36 +0100

    Upgrade SIF to 1.1.0

--- scripts/Install-SIF.ps1
+++ scripts/Install-SIF.ps1
@@ -1,4 +1,4 @@
 # Install SIF
 Install-PackageProvider -Name NuGet -Force; `
 Register-PSRepository -Name SitecoreGallery -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2; `
-Install-Module SitecoreInstallFramework -RequiredVersion 1.0.2 -Force 
+Install-Module SitecoreInstallFramework -RequiredVersion 1.1.0 -Force 
