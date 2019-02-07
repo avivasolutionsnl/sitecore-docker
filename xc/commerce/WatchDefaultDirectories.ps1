b6924b3ac3c80db0b03682b5b49ab286ea9cb1d1
Joost Meijles <joost@meijles.com>
2019-02-07 16:16:05 +0100

    Remove XC

--- xc/commerce/WatchDefaultDirectories.ps1
+++
@@ -1,1 +0,0 @@
-C:\Scripts\WatchDirectoryMultiple.ps1 -Path C:\Workspace -Destinations @('C:\\inetpub\\wwwroot\\CommerceAuthoring_Sc9', 'C:\\inetpub\\wwwroot\\CommerceMinions_Sc9', 'C:\\inetpub\\wwwroot\\CommerceOps_Sc9', 'C:\\inetpub\\wwwroot\\CommerceShops_Sc9')
