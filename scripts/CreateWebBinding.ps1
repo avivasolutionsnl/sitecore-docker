a600f2f3dbd13f9867a31854c2e220684c28fe27
Jason Palios <jason.palios@avivasolutions.nl>
2019-02-15 14:22:06 +0100

    backtick removal

--- scripts/CreateWebBinding.ps1
+++ scripts/CreateWebBinding.ps1
@@ -9,8 +9,8 @@ Param(
     $hostname
 )
 
-$password = ConvertTo-SecureString -String $secret -Force -AsPlainText; `
-$PFXCert = Get-PfxData -FilePath $certpath -Password $password; `
-New-WebBinding -Name $sitename -IPAddress * -Port 443 -Protocol "https" -HostHeader $hostname; `
-$binding = Get-WebBinding -Name $sitename -Protocol "https"; `
+$password = ConvertTo-SecureString -String $secret -Force -AsPlainText;
+$PFXCert = Get-PfxData -FilePath $certpath -Password $password;
+New-WebBinding -Name $sitename -IPAddress * -Port 443 -Protocol "https" -HostHeader $hostname;
+$binding = Get-WebBinding -Name $sitename -Protocol "https";
 $binding.AddSslCertificate($PFXCert.EndEntityCertificates.ThumbPrint, 'my')
