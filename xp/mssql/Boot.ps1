3c81ebdb0b31964093004eb526dfde36b8b5592c
Joost Meijles <joost@meijles.com>
2019-02-22 17:53:29 +0100

    Repair database when it cannot be attached

--- xp/mssql/Boot.ps1
+++ xp/mssql/Boot.ps1
@@ -29,11 +29,19 @@ Get-ChildItem -Path $DataPath -Filter "*.mdf" | ForEach-Object {
     $databaseName = $_.BaseName.Replace("_Primary", "").Replace("_primary", "")
     $mdfPath = $_.FullName
     $ldfPath = $mdfPath.Replace(".mdf", ".ldf")
+
     $sqlcmd = "IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE NAME = '$databaseName') BEGIN EXEC sp_detach_db [$databaseName] END;CREATE DATABASE [$databaseName] ON (FILENAME = N'$mdfPath'), (FILENAME = N'$ldfPath') FOR ATTACH;"
 
     Write-Host "### Attaching '$databaseName'..."
 
-    Invoke-Sqlcmd -Query $sqlcmd
+    try {
+        Invoke-Sqlcmd -Query $sqlcmd
+    } catch {
+        Write-Host "### Repairing '$databaseName'..."
+
+        $repairCmd = "ALTER DATABASE $databaseName SET EMERGENCY; GO DBCC CHECKDB ($databaseName, REPAIR_ALLOW_DATA_LOSS) WITH NO_INFOMSGS; GO";
+        Invoke-Sqlcmd -Query $repairCmd
+    }
 }
 
 # See http://jonnekats.nl/2017/sql-connection-issue-xconnect/
