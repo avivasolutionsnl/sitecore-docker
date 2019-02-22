5a2e78b4b0916d10b1fde10702471396b3ada3ec
Jason Palios <jason.palios@avivasolutions.nl>
2019-02-22 10:51:19 +0100

    port and protocol are variables

--- scripts/CreateWebBinding.ps1
+++ scripts/CreateWebBinding.ps1
@@ -6,11 +6,14 @@ Param(
     [Parameter(Mandatory=$True)]
     $sitename,
     [Parameter(Mandatory=$True)]
-    $hostname
+    $hostname,
+    $port = "443",
+    $protocol = "https"
+
 )
 
 $password = ConvertTo-SecureString -String $secret -Force -AsPlainText;
 $PFXCert = Get-PfxData -FilePath $certpath -Password $password;
-New-WebBinding -Name $sitename -IPAddress * -Port 443 -Protocol "https" -HostHeader $hostname;
-$binding = Get-WebBinding -Name $sitename -Protocol "https";
+New-WebBinding -Name $sitename -IPAddress * -Port $port -Protocol $protocol -HostHeader $hostname;
+$binding = Get-WebBinding -Name $sitename -Protocol $protocol;
 $binding.AddSslCertificate($PFXCert.EndEntityCertificates.ThumbPrint, 'my')
