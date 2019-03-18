4dae557a3ec21780bbe59d284219e7d93f3169c5
Jonne <jonnekats@gmail.com>
2019-03-18 17:13:13 +0100

    Add JSS to XP and XC (#64)
    * Add JSS to XP
    
    * Fix XP Jss scenario to new overlay less approach
    
    * Add JSS to XC scenario
    
    * Add push targets
    
    * Add readme
    
    * Add JSS  to default targets

--- build/Build.cs
+++ build/Build.cs
@@ -37,7 +37,7 @@ partial class Build : NukeBuild
     }
 
     private void AssertCleanDirectory(string dir) {
-        if (System.IO.Directory.Exists(dir)) {
+        if (!System.IO.Directory.Exists(dir)) {
             System.IO.Directory.CreateDirectory(dir);
         } else {
             Nuke.Common.ControlFlow.Assert(
@@ -103,8 +103,8 @@ partial class Build : NukeBuild
     }
 
     Target All => _ => _
-        .DependsOn(Xp, XpSxa, Xc, XcSxa);
+        .DependsOn(Xp, XpSxa, XpJss, Xc, XcSxa, XcJss);
 
     Target Push => _ => _
-        .DependsOn(PushXp, PushXpSxa, PushXc, PushXcSxa);
+        .DependsOn(PushXp, PushXpSxa, PushXpJss, PushXc, PushXcSxa, PushXcJss);
 }
