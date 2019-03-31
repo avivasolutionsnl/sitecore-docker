757d749e849f1ea16fb873e29cc6f45bf12080c7
Joost <joost@meijles.com>
2019-03-31 14:10:34 +0200

    Undo

--- xp/mssql/Install-Databases.ps1
+++ xp/mssql/Install-Databases.ps1
@@ -12,36 +12,31 @@ param(
 )
 
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
-echo "1"
-echo $InstallPath
-echo $env:COMPUTERNAME
+
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
 $server.Properties["DefaultFile"].Value = $InstallPath
 $server.Properties["DefaultLog"].Value = $InstallPath
 $server.Alter()
-echo "2"
+
 $sqlPackageExePath = Get-Item "C:\tools\*\lib\net46\SqlPackage.exe" | Select-Object -Last 1 -Property FullName -ExpandProperty FullName
-echo "3"
+
 Write-Host "Using: $sqlPackageExePath"
 
 Push-Location -Path $InstallPath
 
 (Get-ChildItem -Path $InstallPath -Filter "*.dacpac" | Where-Object { !($_.Name -like "*Xdb*") }) | ForEach-Object {
     $databaseName = $_.BaseName.Replace("Sitecore.", "$DatabasePrefix`_").Replace(".Database.Sql", ".ShardMapManager")
-    echo $databaseName
     $dacpacPath = ".\{0}" -f $_.Name
-    echo $dacpacPath
+
     # Install
     & $sqlPackageExePath /a:Publish /sf:$dacpacPath /tdn:$databaseName /tsn:$env:COMPUTERNAME /q
-    echo "Installed"
+
     # Detach
     Invoke-Sqlcmd -Query "EXEC MASTER.dbo.sp_detach_db @dbname = N'$databaseName', @keepfulltextindexfile = N'false'"
-    echo "Detached"
 }
 
 Pop-Location
-echo "4"
-echo $DataPath
+
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server($env:COMPUTERNAME)
 $server.Properties["DefaultFile"].Value = $DataPath
 $server.Properties["DefaultLog"].Value = $DataPath
