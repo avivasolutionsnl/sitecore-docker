93ec087c5e3f80a0939adecaf932dbdcfbbf8ba3
Joost Meijles <joost@meijles.com>
2019-04-08 14:08:34 +0200

    Enlarge XC SXA timeout to 7200

--- xc/sitecore/sxa/InstallSXA.ps1
+++ xc/sitecore/sxa/InstallSXA.ps1
@@ -9,6 +9,9 @@ $filesPath = "/files-mount"
 # Copy utilities to install packages (this is taken from Sitecore Commerce SIF)
 Copy-Item -Path $env:INSTALL_TEMP/SiteUtilityPages -Destination c:\\inetpub\\wwwroot\\sitecore -Force -Recurse
 
+# Enlarge timeout to 7200 seconds
+((Get-Content -path $env:INSTALL_TEMP/Modules/SitecoreUtilityTasks/SitecoreUtilityTasks.psm1 -Raw) -replace '720','7200') | Set-Content -Path $env:INSTALL_TEMP/Modules/SitecoreUtilityTasks/SitecoreUtilityTasks.psm1
+
 # Install PSE and SXA packages
 Install-SitecoreConfiguration -Path '/sxa/install-sxa.json' `
     -PowershellExtensionPackageFullPath "$filesPath/$env:PSE_PACKAGE" `
