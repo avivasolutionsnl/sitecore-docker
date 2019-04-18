37639b6259a370c33e9a282cc403aa5da8c2dac2
Rick van Dam <rick-van-dam@outlook.com>
2019-04-18 09:49:10 +0200

    wip

--- xc/commerce/UpdateHostname.ps1
+++ xc/commerce/UpdateHostname.ps1
@@ -7,87 +7,134 @@ param(
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
+Function UpdateBindingCertificate() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [X509Certificate]$certificate, 
+        [Parameter(Mandatory = $true)]
+        [string]$bindingName, 
+        [Parameter(Mandatory = $true)]
+        [uint32]$port
+    )
+    Write-Host "Deleting old $bindingName with port $port"
+    Get-WebBinding -Name $bindingName -Protocol 'https' -Port $port | Remove-WebBinding
+
+    Write-Host "Creating new binding $bindingName with port $port"
+    New-WebBinding -Name $bindingName -HostHeader '*' -IPAddress * -Protocol 'https' -Port $port
+    $binding = Get-WebBinding -Name $bindingName -Protocol 'https' -Port $port
+    Write-Host "Succesfully created a new https binding with name $bindingName!" -ForegroundColor Green
+
+    Write-Host "Updating the certificate for binding $bindingName with port $port"
+    $binding.AddSslCertificate($certificate.GetCertHashString(), 'My')
+    Write-Host "Succesfully updated the binding certificate!" -ForegroundColor Green
+}
+
+function UpdateBizFxConfig {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$configPath, 
+        [Parameter(Mandatory = $true)]
+        [string]$bizFxUrl,
+        [Parameter(Mandatory = $true)]
+        [string]$identityServerUrl,
+        [Parameter(Mandatory = $true)]
+        [string]$engineServerUrl
+    )
+    Write-Host "Patching $configPath with $commerceUrl"
+
+    $json = Get-Content $configPath -raw | ConvertFrom-Json; 
+    $json.BizFxUri = $bizFxUrl
+    $json.IdentityServerUri = $identityServerUrl
+    $json.EngineUri = $engineServerUrl
+    $json | ConvertTo-Json | set-content $configPath
+
+    $json = ConvertTo-Json $json -Depth 100
+    Set-Content $configPath -Value $json -Encoding UTF8
+    Write-Host "Done patching $configPath!" -ForegroundColor Green
+}
+
+function UpdateIdentityServerConfig {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$configPath, 
+        [Parameter(Mandatory = $true)]
+        [string[]]$urls,
+        [Parameter(Mandatory = $true)]
+        [string[]]$allowedOrigins
+    )
+    Write-Host "Patching $configPath with $allowedOrigins"
+
+    $json = Get-Content $configPath -raw | ConvertFrom-Json; 
+    foreach ($p in $json.AppSettings.Clients) {
+        if ($p.ClientId -eq "CommerceBusinessTools") {
+            $p.RedirectUris = $urls
+            $p.PostLogoutRedirectUris = $urls
+            $p.AllowedCorsOrigins = $allowedOrigins
+        }
+    }
+
+    $json = ConvertTo-Json $json -Depth 100
+    Set-Content $configPath -Value $json -Encoding UTF8
+    Write-Host "Done patching $configPath!" -ForegroundColor Green
+}
+
+Function UpdateCommerceConfig() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$configPath, 
+        [Parameter(Mandatory = $true)]
+        [string[]]$allowedOrigins,
+        [Parameter(Mandatory = $true)]
+        [string]$identityServerUrl
+    )
+
+    Write-Host "Patching $configPath with $allowedOrigins"
+    $json = Get-Content $configPath -raw | ConvertFrom-Json;
+    $json.AppSettings.AllowedOrigins = $allowedOrigins
+    $json.AppSettings.SitecoreIdentityServerUrl = $identityServerUrl
+    $json = ConvertTo-Json $json -Depth 100
+    Set-Content $configPath -Value $json -Encoding UTF8
+    Write-Host "Done patching $configPath!" -ForegroundColor Green
+}
+
 #Modify the certificate for the binding
-$certificates = Get-ChildItem -Path 'cert:\localmachine\my' -DnsName 'DO_NOT_TRUST_SitecoreRootCert';
+[X509Certificate[]]$certificates = Get-ChildItem -Path 'cert:\localmachine\my' -DnsName 'DO_NOT_TRUST_SitecoreRootCert';
 $hostnames = [string[]]@(
     ("$hostname")
 )
 
-$rootCert = $certificates[0];
+[X509Certificate]$rootCert = $certificates[0];
 Write-Host "Creating a new certificate with hostnames $hostnames"
-$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $hostnames -Signer $rootcert -KeyExportPolicy Exportable -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider';
+[X509Certificate]$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $hostnames -Signer $rootcert -KeyExportPolicy Exportable -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider';
 Write-Host "Succesfully created the certificate!. Updating bindings..." -ForegroundColor Green
 
-$updateBindingCertifcate = "$PSScriptRoot/UpdateBindingCertificate.ps1"
+UpdateBindingCertificate -certificate $certificate -bindingName 'CommerceOps_Sc9' -port 5015
+UpdateBindingCertificate -certificate $certificate -bindingName 'CommerceShops_Sc9' -port 5005
+UpdateBindingCertificate -certificate $certificate -bindingName 'CommerceAuthoring_Sc9' -port 5000 
+UpdateBindingCertificate -certificate $certificate -bindingName 'CommerceMinions_Sc9'-port 5010
+UpdateBindingCertificate -certificate $certificate -bindingName 'SitecoreBizFx' -port 4200
+UpdateBindingCertificate -certificate $certificate -bindingName 'SitecoreIdentityServer' -port 5050
 
-& $updateBindingCertifcate -certificate $certificate -bindingName 'CommerceOps_Sc9' -port 5015
-& $updateBindingCertifcate -certificate $certificate -bindingName 'CommerceShops_Sc9' -port 5005
-& $updateBindingCertifcate -certificate $certificate -bindingName 'CommerceAuthoring_Sc9' -port 5000 
-& $updateBindingCertifcate -certificate $certificate -bindingName 'CommerceMinions_Sc9'-port 5010
-& $updateBindingCertifcate -certificate $certificate -bindingName 'SitecoreBizFx' -port 4200
-& $updateBindingCertifcate -certificate $certificate -bindingName 'SitecoreIdentityServer' -port 5050
-
-$baseUrl = "https://$hostname"
-$identityServerUrl = $baseUrl + ":5050"
-$engineServerUrl = $baseUrl + ":5000"
-$bizFxUrl = $baseUrl + ":4200"
-$urls = @(
+[string]$baseUrl = "https://$hostname"
+[string]$identityServerUrl = $baseUrl + ":5050"
+[string]$engineServerUrl = $baseUrl + ":5000"
+[string]$bizFxUrl = $baseUrl + ":4200"
+[string]$plumberUrl = $baseUrl + ":4000"
+$urls = [string[]]@(
     ($bizFxUrl),
     ($bizFxUrl + "/?")
 )
-$allowedOrigins = @(
+$allowedOrigins = [string[]]@(
     "http://sitecore",
     ($bizFxUrl),
-    ($bizFxUrl + "/?")
+    ($bizFxUrl + "/?"),
+    $plumberUrl
 )
-#Patch the bizfx config
-$pathToBizfxConfig = "C:\inetpub\wwwroot\SitecoreBizFx\assets\config.json"
-Write-Host "Patching $pathToBizfxConfig with $commerceUrl"
 
-$json = Get-Content $pathToBizfxConfig -raw | ConvertFrom-Json; 
-$json.BizFxUri = $bizFxUrl
-$json.IdentityServerUri = $identityServerUrl
-$json.EngineUri = $engineServerUrl
-$json | ConvertTo-Json | set-content $pathToBizfxConfig
+UpdateBizFxConfig -configPath "C:\inetpub\wwwroot\SitecoreBizFx\assets\config.json" -bizFxUrl $bizFxUrl -identityServerUrl $identityServerUrl -engineServerUrl $engineServerUrl
+UpdateIdentityServerConfig -configPath "C:\inetpub\wwwroot\SitecoreIdentityServer\wwwroot\appsettings.json" -urls $urls -allowedOrigins $allowedOrigins
 
-$json = ConvertTo-Json $json -Depth 100
-Set-Content $pathToBizfxConfig -Value $json -Encoding UTF8
-Write-Host "Done patching $pathToBizfxConfig!" -ForegroundColor Green
-
-#Patch the sitecore identity server config
-$pathToIdentityServerConfig = "C:\inetpub\wwwroot\SitecoreIdentityServer\wwwroot\appsettings.json"
-
-Write-Host "Patching $pathToIdentityServerConfig with $urls"
-
-$json = Get-Content $pathToIdentityServerConfig -raw | ConvertFrom-Json; 
-foreach ($p in $json.AppSettings.Clients) {
-    if($p.ClientId -eq "CommerceBusinessTools"){
-        $p.RedirectUris = $urls
-        $p.PostLogoutRedirectUris = $urls
-        $p.AllowedCorsOrigins = $urls
-    }
-}
-
-$json = ConvertTo-Json $json -Depth 100
-Set-Content $pathToIdentityServerConfig -Value $json -Encoding UTF8
-Write-Host "Done patching $pathToIdentityServerConfig!" -ForegroundColor Green
-
-#Patch the commerce engine config
-$pathToCommerceEngineConfig = "C:\inetpub\wwwroot\CommerceAuthoring_Sc9\wwwroot\config.json"
-Write-Host "Patching $pathToCommerceEngineConfig with $urls"
-$json = Get-Content $pathToCommerceEngineConfig -raw | ConvertFrom-Json;
-$json.AppSettings.AllowedOrigins = $urls
-$json.AppSettings.SitecoreIdentityServerUrl = $identityServerUrl
-$json = ConvertTo-Json $json -Depth 100
-Set-Content $pathToCommerceEngineConfig -Value $json -Encoding UTF8
-Write-Host "Done patching $pathToCommerceEngineConfig!" -ForegroundColor Green
-
-#Patch the commerce ops config
-$pathToCommerceOpsConfig = "C:\inetpub\wwwroot\CommerceOps_Sc9\wwwroot\config.json"
-Write-Host "Patching $pathToCommerceOpsConfig with $urls"
-$json = Get-Content $pathToCommerceOpsConfig -raw | ConvertFrom-Json;
-$json.AppSettings.AllowedOrigins = $urls
-$json.AppSettings.SitecoreIdentityServerUrl = $identityServerUrl
-$json = ConvertTo-Json $json -Depth 100
-Set-Content $pathToCommerceOpsConfig -Value $json -Encoding UTF8
-Write-Host "Done patching $pathToCommerceOpsConfig!" -ForegroundColor Green
+UpdateCommerceConfig -configPath "C:\inetpub\wwwroot\CommerceAuthoring_Sc9\wwwroot\config.json" -allowedOrigins $allowedOrigins -identityServerUrl $identityServerUrl
+UpdateCommerceConfig -configPath "C:\inetpub\wwwroot\CommerceOps_Sc9\wwwroot\config.json" -allowedOrigins $allowedOrigins -identityServerUrl $identityServerUrl
+UpdateCommerceConfig -configPath "C:\inetpub\wwwroot\CommerceMinions_Sc9\wwwroot\config.json" -allowedOrigins $allowedOrigins -identityServerUrl $identityServerUrl
+UpdateCommerceConfig -configPath "C:\inetpub\wwwroot\CommerceShops_Sc9\wwwroot\config.json" -allowedOrigins $allowedOrigins -identityServerUrl $identityServerUrl
