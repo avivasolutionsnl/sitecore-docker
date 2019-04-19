28dc6d009ffce22bb9d8a641e9722c0d44db38db
Rick van Dam <Barsonax@users.noreply.github.com>
2019-04-19 11:56:22 +0200

    MCY-2785 start xconnect from the boot script (#103)
    * xconnect is now started from the boot script
    
    * renamed boot.ps1 to Boot.ps1 (casing)
    
    * added more logging

--- xp/xconnect/Boot.ps1
+++ xp/xconnect/Boot.ps1
@@ -5,10 +5,16 @@ if(Test-Path $licenseFile)
     Copy-Item -Path $licenseFile -Destination "C:/inetpub/wwwroot/xconnect/App_Data"
     Copy-Item -Path $licenseFile -Destination "C:/inetpub/wwwroot/xconnect/App_Data/jobs/continuous/AutomationEngine/App_Data"
     Copy-Item -Path $licenseFile -Destination "C:/inetpub/wwwroot/xconnect/App_Data/jobs/continuous/IndexWorker/App_Data"
+    Write-Host "Succesfully copied license file!" -ForegroundColor Green
+
+    Write-Host "Starting the xconnect services"
+    Start-Service "xconnect-IndexWorker"
+    Start-Service "xconnect-MarketingAutomationService"
+    Write-Host "Succesfully started the xconnect services" -ForegroundColor Green
 }
 else
 {
-    Write-Host "No license file found. Please put in a license.xml file in the /license folder"
+    Write-Host "No license file found. Please put in a license.xml file in the /license folder. The container will now exit." -ForegroundColor Red
     exit
 }
 C:\ServiceMonitor.exe w3svc
