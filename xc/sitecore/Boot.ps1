7966e6408c64262eb155032fb82e5a1564929b48
Jonne Kats <jonne.kats@avivasolutions.nl>
2019-05-02 13:32:34 +0200

    Fix dynamic hostnames

--- xc/sitecore/Boot.ps1
+++ xc/sitecore/Boot.ps1
@@ -1,9 +1,11 @@
 param(
     [Parameter(Mandatory=$true)]
-    [String]$commerceHostname
+    [String]$commerceHostname,
+    [Parameter(Mandatory=$true)]
+    [String]$identityHostname
 )
 
-/Scripts/UpdateHostname.ps1 -commerceHostname $commerceHostname
+/Scripts/UpdateHostnames.ps1 -commerceHostname $commerceHostname -identityHostname $identityHostname
 
 If ((Test-Path C:\Workspace) -eq $False) { 
     New-Item -Type Directory c:\Workspace  
