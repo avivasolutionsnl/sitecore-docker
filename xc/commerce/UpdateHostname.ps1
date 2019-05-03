439e6439b540c98929e7c71a6b23351c3e47b04d
Jonne Kats <jonne.kats@avivasolutions.nl>
2019-05-03 13:53:52 +0200

    Restructure certificates and use SHA256
    Chrome was not accepting the certificates because they were SHA1. Not all certificated were signed by the root certificate.

--- xc/commerce/UpdateHostname.ps1
+++ xc/commerce/UpdateHostname.ps1
@@ -118,7 +118,8 @@ Write-Host "Succesfully updated hostfile!" -ForegroundColor Green
 [X509Certificate[]]$certificates = Get-ChildItem -Path 'cert:\localmachine\root' -DnsName 'sitecore-docker-devonly';
 [X509Certificate]$rootCert = $certificates[0];
 Write-Host "Creating a new certificate with hostnames $commerceHostname"
-[X509Certificate]$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $commerceHostname -Signer $rootcert -KeyUsage CertSign,CRLSign,DataEncipherment,DigitalSignature,KeyAgreement,KeyEncipherment -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider';
+[X509Certificate]$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $commerceHostname -Signer $rootcert -KeyUsage CertSign,CRLSign,DataEncipherment,DigitalSignature,KeyAgreement,KeyEncipherment -Provider "Microsoft Strong Cryptographic Provider" `
+-HashAlgorithm "SHA256"
 Write-Host "Succesfully created the certificate!. Updating bindings..." -ForegroundColor Green
 
 UpdateBindingCertificate -certificate $certificate -bindingName 'CommerceOps_Sc9' -port 5015
