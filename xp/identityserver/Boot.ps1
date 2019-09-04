7b46a42cc96bdf9bf9303c44e74c4a958bf07212
Joost <joost@meijles.com>
2019-09-04 11:47:18 +0200

    Externalize Identity server license (#149)
    * Externalize Identity server license
    
    * Undo process isolation removal
    
    * Mount license
    
    * Add custom config file
    
    * Fix typo

--- xp/identityserver/Boot.ps1
+++ xp/identityserver/Boot.ps1
@@ -5,6 +5,17 @@ param(
     [String]$identityHostname
 )
 
+$licenseFile = "C:/license/license.xml"
+if(Test-Path $licenseFile)
+{
+    Write-Host "Found license file"
+}
+else
+{
+    Write-Host "No license file found. Please put in a license.xml file in the /license folder"
+    exit
+}
+
 /Scripts/UpdateHostname.ps1 -sitecoreHostname $sitecoreHostname -identityHostname $identityHostname
 
 while($true)
